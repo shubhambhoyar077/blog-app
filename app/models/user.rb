5237fdc57fe2e6bee11fdec4d32b8fd159022d6a
shubham <shubham.bhoyar077@gmail.com>
2023-06-07 17:16:55 +0530

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :post, foreign_key: "author_id"
-  has_many :like, foreign_key: "author_id"
-  has_many :comment, foreign_key: "author_id"
+  has_many :post, foreign_key: 'author_id'
+  has_many :like, foreign_key: 'author_id'
+  has_many :comment, foreign_key: 'author_id'
 
   def top_3_posts
     post.order(created_at: :desc).limit(3)

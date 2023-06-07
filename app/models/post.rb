4f8c7f18383aa4f0ef8847e36a8f46d6ecb9d036
shubham <shubham.bhoyar077@gmail.com>
2023-06-07 16:46:14 +0530

    add class name and foreign key

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user, foreign_key: "author_id", counter_cache: :post_counter
-  has_many :user, through: :like
-  has_many :user, through: :comment
+  belongs_to :author, class_name: 'User', counter_cache: :post_counter
+  has_many :like
+  has_many :comment
 end

c9b5f9ccd81d1d4e2b63cb4e36cbe03721c93adc
shubham <shubham.bhoyar077@gmail.com>
2023-06-07 17:15:04 +0530

    Add comment couter method for post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post, counter_cache: :comments_counter
+
+  def increment_comment_counter
+    post.increment!(:comments_counter)
+  end
 end

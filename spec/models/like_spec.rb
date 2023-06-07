a5a42256f00e092a9e112262593d468e45a91d99
shubham <shubham.bhoyar077@gmail.com>
2023-06-07 21:58:48 +0530

    Fix linters

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 describe Like, type: :model do
-    test_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos', bio: 'Teacher from Mexico.')
-    test_post = Post.create(author: test_user, title: 'Hello', text: 'This is my first post')
-    test_like = Like.new(post: test_post, author: test_user)
+  test_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos', bio: 'Teacher from Mexico.')
+  test_post = Post.create(author: test_user, title: 'Hello', text: 'This is my first post')
+  test_like = Like.new(post: test_post, author: test_user)
 
-    before {test_like.save}
-    it "like should increase user like counter by 1" do
-      #it will increase automatically after like save
-      expect(test_post.likes_counter).to eq(1)
-    end
-
+  before { test_like.save }
+  it 'like should increase user like counter by 1' do
+    # it will increase automatically after like save
+    expect(test_post.likes_counter).to eq(1)
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,49 +1,48 @@
 require 'rails_helper'
 
 describe Post, type: :model do
-    test_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos', bio: 'Teacher from Mexico.')
-    test_post = Post.new(author: test_user, title: 'Hello', text: 'This is my first post')
+  test_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos', bio: 'Teacher from Mexico.')
+  test_post = Post.new(author: test_user, title: 'Hello', text: 'This is my first post')
 
-  before {test_post.save}
-  it "Title should be present" do
+  before { test_post.save }
+  it 'Title should be present' do
     test_post.title = nil
     expect(test_post).to_not be_valid
   end
 
-  it "comment counter should be integer" do
+  it 'comment counter should be integer' do
     subject.comments_counter = 1.5
     expect(subject).to_not be_valid
   end
 
-  it "comment counter should be greated than zero" do
+  it 'comment counter should be greated than zero' do
     subject.comments_counter = -1
     expect(subject).to_not be_valid
   end
 
-  it "like counter should be integer" do
+  it 'like counter should be integer' do
     subject.likes_counter = 1.5
     expect(subject).to_not be_valid
   end
 
-  it "like counter should be greated than zero" do
+  it 'like counter should be greated than zero' do
     subject.likes_counter = -1
     expect(subject).to_not be_valid
   end
-  
-  it "post should increase user post counter by 1" do
-    #it will increase automatically after post save
+
+  it 'post should increase user post counter by 1' do
+    # it will increase automatically after post save
     expect(test_user.post_counter).to eq(1)
   end
 
-  it "post should return top 5 post" do
+  it 'post should return top 5 post' do
     test_post = Post.create(author: test_user, title: 'Hello', text: 'This is my first post')
     Comment.create(post: test_post, author: test_user, text: 'This is my first comment')
     Comment.create(post: test_post, author: test_user, text: 'This is my 2nd comment')
     Comment.create(post: test_post, author: test_user, text: 'This is my 3rd comment')
     Comment.create(post: test_post, author: test_user, text: 'This is my 4th comment')
     Comment.create(post: test_post, author: test_user, text: 'This is my 5th comment')
-    
-    
+
     expect(test_post.top_5_comment.size).to eq(5)
     expect(test_post.top_5_comment[0].text).to eq('This is my 5th comment')
     expect(test_post.top_5_comment[1].text).to eq('This is my 4th comment')
@@ -51,5 +50,4 @@ describe Post, type: :model do
     expect(test_post.top_5_comment[3].text).to eq('This is my 2nd comment')
     expect(test_post.top_5_comment[4].text).to eq('This is my first comment')
   end
-
 end

bd8f1a28a9e1d25be45822b6c6f1ad1a790caaee
shubham <shubham.bhoyar077@gmail.com>
2023-06-07 16:48:45 +0530

    Add method for return top 5 comment in given post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', counter_cache: :post_counter
   has_many :like
   has_many :comment
+
+  scope :post_top_5_comment, ->(post) {post.comment.order(created_at: :desc).limit(5)}
 end

a12a946a7b835878d91309dd8548765aaf548cf7
shubham <shubham.bhoyar077@gmail.com>
2023-06-12 18:48:27 +0530

    Add test for urls /user/:id

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -19,4 +19,28 @@ RSpec.describe "Users", type: :request do
     expect(response).to render_template('index')
   end
   end
+
+  describe "GET /users/:id" do
+    before :all do
+      @new_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos', bio: 'Teacher from Mexico.')
+    end
+    it 'should responce with success.' do
+      
+      # new_user = User.create! valid_attributes
+      get user_url(@new_user)
+      expect(response).to have_http_status(:success)
+    end
+    it 'should responce with correct status' do
+      get user_url(@new_user)
+      expect(response.status).to eq(200)
+    end
+    it 'should include correct placeholder text.' do
+      get user_url(@new_user)
+      expect(response.body).to include("User details")
+  end
+  it 'should render correct template' do
+    get user_url(@new_user)
+    expect(response).to render_template('show')
+  end
+  end
 end

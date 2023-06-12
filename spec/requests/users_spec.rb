d23839eb04535ad1d94ca03b86b198176e46f5cf
shubham <shubham.bhoyar077@gmail.com>
2023-06-12 18:32:05 +0530

    Add test for /user url

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,14 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    it 'test' do
-      get users_path
-      expect(page).to have_content("j")
-    end
-    it 'test' do
+  describe "GET /users" do
+    it 'should responce with success.' do
       get users_path
       expect(response).to have_http_status(:success)
     end
+    it 'should responce with correct status' do
+      get users_path
+      expect(response.status).to eq(200)
+    end
+    it 'should include correct placeholder text.' do
+      get users_path
+      expect(response.body).to include("Users List")
+  end
+  it 'should render correct template' do
+    get users_path
+    expect(response).to render_template('index')
+  end
   end
 end

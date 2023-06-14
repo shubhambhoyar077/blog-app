d2c2f7b279309376f9006bb100029c9aeba0d49d
shubham <shubham.bhoyar077@gmail.com>
2023-06-14 19:37:29 +0530

    Add logic for create comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
+  def create
+    post = Post.find(params[:post_id])
+    @comment = post.comment.new(author: current_user, **comment_param)
+    if @comment.save
+      redirect_to @comment
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def comment_param
+    params.require(:comment).permit(:text)
+  end
 end

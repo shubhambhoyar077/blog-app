a0fffbfca2c3c878a2e57769815203ba13ccde33
shubham <shubham.bhoyar077@gmail.com>
2023-06-14 21:32:23 +0530

    Remove new method from controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,10 @@
 class CommentsController < ApplicationController
-  def new
-    @post = Post.find(params[:post_id])
-    @comment = Comment.new
-  end
 
   def create
     post = Post.find(params[:post_id])
     @comment = post.comment.new(author: current_user, **comment_param)
     if @comment.save
-      redirect_to @comment
+      redirect_to user_post_path(current_user, post)
     else
       render :new, status: :unprocessable_entity
     end

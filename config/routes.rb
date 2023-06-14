284b41dc5d797a2e826b5f4248e72f2125fc98ac
shubham <shubham.bhoyar077@gmail.com>
2023-06-14 18:12:30 +0530

    Add routes for create post and comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "users#index"
-  resources :users, only: [:index, :show] do
+  resources :users, only: [:show] do
     resources :posts, only: [:index, :show]
+  end
+
+  resources :posts, only: [:new, :create] do
+    resources :comments, only: [:new, :create]
   end
 end

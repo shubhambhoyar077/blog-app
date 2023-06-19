d7a5d4e7291ea647ae83856cf7d0480724082f6e
shubham <shubham.bhoyar077@gmail.com>
2023-06-19 17:14:16 +0530

    Add test for number of post and redirect to show page

--- spec/integration/user_spec.rb
+++ spec/integration/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Users', type: :system do
   describe 'index page' do
     before do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(User.create(name: 'Tom', photo: 'https://unsplash.com/photos', bio: 'Teacher from Mexico.'))
-      User.create(name: 'Shubham', photo: 'https://unsplash.com/photos', bio: 'Teacher from Mexico.')
+      @first_user = User.create(name: 'Shubham', photo: 'https://unsplash.com/photos', bio: 'Teacher from Mexico.')
       User.create(name: 'Enis', photo: 'https://unsplash.com/photos', bio: 'Teacher from Mexico.')
       visit root_path
     end
@@ -20,6 +20,15 @@ RSpec.describe 'Users', type: :system do
       expect(page).to have_css("img[src='https://unsplash.com/photos']")
       expect(page).to have_css("img[src='https://unsplash.com/photos']")
       expect(page).to have_css("img[src='https://unsplash.com/photos']")
+    end
+    it 'I can see the number of posts each user has written.' do
+      Post.create(author: @first_user, title: 'Hello', text: 'This is my first post')
+      expect(page).to have_content("Number of posts: 1")
+      expect(page).to have_content("Number of posts: 0")
+    end
+    it 'When I click on a user, I am redirected to that user show page.' do
+      click_link 'Shubham'
+      expect(page).to have_current_path(user_path(@first_user))
     end
   end
 end

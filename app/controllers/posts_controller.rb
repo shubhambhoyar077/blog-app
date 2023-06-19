5f4695cd021d37f0e86ec9765e11780808908f0a
shubham <shubham.bhoyar077@gmail.com>
2023-06-19 14:43:20 +0530

    Using include to solve n+1 problem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
-    @posts = Post.where(author_id: params[:user_id])
+    @user = User.includes(post: :comment).find(params[:user_id])
+    # @posts = Post.includes(:comment).where(author_id: params[:user_id])
   end
 
   def show

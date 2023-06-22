88ddc1281a3b53331b4a5da1af2dfc4cd3a5a8f3
shubham <shubham.bhoyar077@gmail.com>
2023-06-22 18:36:47 +0530

    Fix linters

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -2,30 +2,28 @@ class Api::CommentsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    begin
-      post = Post.find(params[:post_id])
-      comments = post.comment
-      render json: comments
-    rescue ActiveRecord::RecordNotFound
-      render json: { error: 'Post not found' }, status: :not_found
-    rescue => e
-      render json: { error: e.message }, status: :unprocessable_entity
+    post = Post.find(params[:post_id])
+    comments = post.comment
+    render json: comments
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Post not found' }, status: :not_found
+  rescue StandardError => e
+    render json: { error: e.message }, status: :unprocessable_entity
   end
 
   def create
-    begin
-      post = Post.find(params[:post_id])
-      comment = post.comment.new(comment_params)
-      comment.author = current_user
-      if comment.save
-        render json: comment, status: :created
-      else
-        render json: { errors: comment.errors.full_messages }, status: :unprocessable_entity
-      end
-    rescue ActiveRecord::RecordNotFound
-      render json: { error: 'Post not found' }, status: :not_found
-    rescue => e
-      render json: { error: e.message }, status: :unprocessable_entity
+    post = Post.find(params[:post_id])
+    comment = post.comment.new(comment_params)
+    comment.author = current_user
+    if comment.save
+      render json: comment, status: :created
+    else
+      render json: { errors: comment.errors.full_messages }, status: :unprocessable_entity
+    end
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Post not found' }, status: :not_found
+  rescue StandardError => e
+    render json: { error: e.message }, status: :unprocessable_entity
   end
 
   private
--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,13 +1,11 @@
 class Api::PostsController < ApplicationController
   def index
-    begin
-      user = User.find(params[:user_id])
-      posts = user.post
-      render json: posts
-    rescue ActiveRecord::RecordNotFound
-      render json: { error: 'User not found' }, status: :not_found
-    rescue => e
-      render json: { error: e.message }, status: :unprocessable_entity
-    end
+    user = User.find(params[:user_id])
+    posts = user.post
+    render json: posts
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'User not found' }, status: :not_found
+  rescue StandardError => e
+    render json: { error: e.message }, status: :unprocessable_entity
   end
 end

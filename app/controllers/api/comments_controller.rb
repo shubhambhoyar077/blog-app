c9cd6f724f135d28577ff25973cbb99c9e7157cf
shubham <shubham.bhoyar077@gmail.com>
2023-06-22 18:30:02 +0530

    Create get and post api call for comment

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,7 +1,26 @@
 class Api::CommentsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     post = Post.find(params[:post_id])
     comments = post.comment
     render json: comments
+  end
+
+  def create
+    post = Post.find(params[:post_id])
+    comment = post.comment.new(comment_params)
+    comment.author = current_user
+    if comment.save
+      render json: comment, status: :created
+    else
+      render json: { errors: comment.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end

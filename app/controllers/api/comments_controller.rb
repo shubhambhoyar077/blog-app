4fdb6cc21629e436f501af3f705fa10175d00953
shubham <shubham.bhoyar077@gmail.com>
2023-06-22 18:31:39 +0530

    Add error handling in comment api

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -2,20 +2,30 @@ class Api::CommentsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    post = Post.find(params[:post_id])
-    comments = post.comment
-    render json: comments
+    begin
+      post = Post.find(params[:post_id])
+      comments = post.comment
+      render json: comments
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'Post not found' }, status: :not_found
+    rescue => e
+      render json: { error: e.message }, status: :unprocessable_entity
   end
 
   def create
-    post = Post.find(params[:post_id])
-    comment = post.comment.new(comment_params)
-    comment.author = current_user
-    if comment.save
-      render json: comment, status: :created
-    else
-      render json: { errors: comment.errors.full_messages }, status: :unprocessable_entity
-    end
+    begin
+      post = Post.find(params[:post_id])
+      comment = post.comment.new(comment_params)
+      comment.author = current_user
+      if comment.save
+        render json: comment, status: :created
+      else
+        render json: { errors: comment.errors.full_messages }, status: :unprocessable_entity
+      end
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'Post not found' }, status: :not_found
+    rescue => e
+      render json: { error: e.message }, status: :unprocessable_entity
   end
 
   private

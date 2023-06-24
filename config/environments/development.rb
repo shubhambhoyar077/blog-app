df0f510a798a768de245aeb22b2f74c1a5645381
shubham <shubham.bhoyar077@gmail.com>
2023-06-24 19:34:51 +0530

    Add mailer smtp setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,5 +69,26 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   #devise default_url
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  #Mailer setup
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:      ENV['SENDMAIL_USERNAME'],
+    password:       ENV['SENDMAIL_PASSWORD'],
+    domain:         ENV['MAIL_HOST'],
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
+  #How to setup ENV variable.
+  # export SENDMAIL_PASSWORD=password
+  # export SENDMAIL_USERNAME=KHongSanFrancisco@gmail.com
+  # export MAIL_HOST=localhost:3000
+
 end
